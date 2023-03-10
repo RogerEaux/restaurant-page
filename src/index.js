5a5dd0511d57c74cd0d024d86e2feab483c34479
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-03-09 22:52:46 -0600

    Add function to change style of selected nav tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './style.css';
 import createElements from './initial-load';
 
+const changeSelected = () => {
+  const nav = document.querySelectorAll('nav > div');
+
+  nav.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      nav.forEach((navtab) => {
+        navtab.classList.remove('selected');
+      });
+      tab.classList.add('selected');
+    });
+  });
+};
+
 createElements();
+changeSelected();
