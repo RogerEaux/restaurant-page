ddbc851cfd48f50f81d2088eb9ea70a3e35eab90
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-03-09 22:01:35 -0600

    Change home div function to separate module

---
+++ src/home.js
@@ -0,0 +1,35 @@
+import flagImage from './images/italy-flag.svg';
+import heartImage from './images/heart.svg';
+
+const createLogo = () => {
+  const logo = document.createElement('div');
+  const flag = document.createElement('img');
+  const heart = document.createElement('img');
+
+  logo.classList.add('logo');
+  flag.src = flagImage;
+  flag.alt = 'Anatomically correct drawn heart';
+  heart.src = heartImage;
+  heart.alt = "Italy's flag";
+  logo.appendChild(flag);
+  logo.appendChild(heart);
+
+  return logo;
+};
+
+const createHome = () => {
+  const home = document.createElement('div');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+
+  p1.textContent = 'Cucinato con il cuore';
+  p2.textContent = 'Autentica pizza italiana';
+  home.appendChild(p1);
+  home.appendChild(createLogo());
+  home.appendChild(p2);
+  home.classList.add('home');
+
+  return home;
+};
+
+export default createHome;
--- src/initial-load.js
+++ src/initial-load.js
@@ -1,5 +1,4 @@
-import flagImage from './images/italy-flag.svg';
-import heartImage from './images/heart.svg';
+import createHome from './home';
 
 const createHeader = () => {
   const header = document.createElement('header');
@@ -27,37 +26,6 @@ const createNav = () => {
   nav.appendChild(contact);
 
   return nav;
-};
-
-const createLogo = () => {
-  const logo = document.createElement('div');
-  const flag = document.createElement('img');
-  const heart = document.createElement('img');
-
-  logo.classList.add('logo');
-  flag.src = flagImage;
-  flag.alt = 'Anatomically correct drawn heart';
-  heart.src = heartImage;
-  heart.alt = "Italy's flag";
-  logo.appendChild(flag);
-  logo.appendChild(heart);
-
-  return logo;
-};
-
-const createHome = () => {
-  const home = document.createElement('div');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p');
-
-  p1.textContent = 'Cucinato con il cuore';
-  p2.textContent = 'Autentica pizza italiana';
-  home.appendChild(p1);
-  home.appendChild(createLogo());
-  home.appendChild(p2);
-  home.classList.add('home');
-
-  return home;
 };
 
 const createMain = () => {
