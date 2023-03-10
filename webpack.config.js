8be29b826c2bc1499b48b507679196be70526cb4
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-03-09 23:17:12 -0600

    Change generated html index file title

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Restaurant page',
+      title: 'Cuore',
       favicon: './src/images/icon.svg',
     }),
   ],
